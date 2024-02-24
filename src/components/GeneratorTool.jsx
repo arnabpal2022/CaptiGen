248081d6828e3e4bba9b010881d8c563b89c65dc
Arnab Pal <arnabpalofficial2003@gmail.com>
2024-02-24 20:03:41 +0530

    feat: created styles

--- src/components/GeneratorTool.jsx
+++ src/components/GeneratorTool.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
+import DateTime from "react-datetime";
 
 function GeneratorTool() {
+  const [changeButton, setChangeButton] = useState(true);
   return (
     <>
       <Header />
       <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <div class="grid md:grid-cols-5 gap-10">
-          <div class="md:col-span-2">Hello World</div>
+          <div class="md:col-span-2">Hello World
+          </div>
           <div class="md:col-span-3">This is Another Section</div>
         </div>
       </div>
--- src/components/subgen.jsx
+++ src/components/subgen.jsx
@@ -3,9 +3,10 @@ import WebVTT from "node-webvtt";
 import DateTime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 import moment from "moment";
-import { parse } from "postcss";
+
 import { TiDelete } from "react-icons/ti";
 import { MdOutlineEdit } from "react-icons/md";
+
 const SubGen = () => {
   const [subtitles, setSubtitles] = useState([]);
   const [startTime, setStartTime] = useState("");
@@ -97,123 +98,218 @@ const SubGen = () => {
       if (index === id) {
         return item;
       }
-    })
+    });
     let startTime = convertSecondsToTime(editItem.startTime);
-    let endTime = convertSecondsToTime(editItem.endTime)
-    
+    let endTime = convertSecondsToTime(editItem.endTime);
+
     setSubtitleText(editItem.text);
     setStartTime(startTime);
     setEndTime(endTime);
-    
+
     setChangeButton(false);
     setEditedItem(id);
-
-  }
+  };
   const handleEditedItem = () => {
     setSubtitles(
       subtitles.map((subtitle, index) => {
-        if(index === editedItem){
-          return {...subtitle, startTime:convertToSeconds(startTime), endTime:convertToSeconds(endTime),  text: subtitleText};
+        if (index === editedItem) {
+          return {
+            ...subtitle,
+            startTime: convertToSeconds(startTime),
+            endTime: convertToSeconds(endTime),
+            text: subtitleText,
+          };
         }
         return subtitle;
       })
-    )
-    
+    );
+
     setChangeButton(true);
-    setSubtitleText("")
-  }
+    setSubtitleText("");
+  };
   const handleDelete = (id) => {
     setSubtitles((items) => {
       return items.filter((arr, index) => {
         return index != id;
-      })
-    })
-  }
+      });
+    });
+  };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div>
-        <div className="text-3xl font-bold">Subtitle Generator</div>
+    <>
+      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div class="grid md:grid-cols-5 gap-10">
+          <div class="md:col-span-2">
+            <div className="text-3xl font-bold">Subtitle Generator</div>
+            <div>
+              <div>
+                <label className="text-bold">Start Time</label>
+                <DateTime
+                  className="border rounded p-2"
+                  value={startTime}
+                  onChange={handleStartTimeChange}
+                  dateFormat={false}
+                  timeFormat="HH:mm:ss"
+                />
+              </div>
+              <div>
+                <label className="text-bold">End Time</label>
+                <DateTime
+                  className="border rounded-xl p-2 block"
+                  value={endTime}
+                  onChange={handleEndTimeChange}
+                  dateFormat={false}
+                  timeFormat="HH:mm:ss"
+                />
+              </div>
+              <label className="font-bold block"> Subtitles :</label>
+              <textarea
+                className="border rounded-xl p-2 w-full block"
+                rows={4}
+                cols={30}
+                placeholder="Write Captions in the Timespan..."
+                value={subtitleText}
+                onChange={handleSubtitleTextChange}
+              />
+              {changeButton ? (
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4 transition duration-300"
+                  onClick={handleAddSubtitle}
+                >
+                  Add Subtitle
+                </button>
+              ) : (
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4 transition duration-300"
+                  onClick={handleEditedItem}
+                >
+                  Submit changes
+                </button>
+              )}
+            </div>
+          </div>
+          <div class="md:col-span-3">
+            <div>
+              <h2 className="font-bold text-xl mt-4">Subtitles :</h2>
+              {subtitles.map((subtitle, index) => (
+                <div key={index} className="flex">
+                  <p className="font-bold w-80 max-w-3/4">
+                    [{convertSecondsToTime(subtitle.startTime)} -{" "}
+                    {convertSecondsToTime(subtitle.endTime)}] : {subtitle.text}
+                  </p>
+                  <button
+                    className="ml-6 text-2xl"
+                    onClick={() => handleEdit(index)}
+                  >
+                    <MdOutlineEdit />
+                  </button>
+                  <button
+                    className="text-2xl"
+                    onClick={() => handleDelete(index)}
+                  >
+                    <TiDelete />
+                  </button>
+                </div>
+              ))}
+            </div>
+            {subtitles.length > 0 && (
+              <div>
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 mt-4"
+                  onClick={handleGenerateSubtitleFile}
+                >
+                  Download Subtitles File
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* <div className="flex justify-center items-center h-screen">
         <div>
+          <div className="text-3xl font-bold">Subtitle Generator</div>
           <div>
-            <label className="text-bold">Start Time</label>
-            <DateTime
-              className="border rounded p-2"
-              value={startTime}
-              onChange={handleStartTimeChange}
-              dateFormat={false}
-              timeFormat="HH:mm:ss"
+            <div>
+              <label className="text-bold">Start Time</label>
+              <DateTime
+                className="border rounded p-2"
+                value={startTime}
+                onChange={handleStartTimeChange}
+                dateFormat={false}
+                timeFormat="HH:mm:ss"
+              />
+            </div>
+            <div>
+              <label className="text-bold">End Time</label>
+              <DateTime
+                className="border rounded-xl p-2 block"
+                value={endTime}
+                onChange={handleEndTimeChange}
+                dateFormat={false}
+                timeFormat="HH:mm:ss"
+              />
+            </div>
+            <label className="font-bold block"> Subtitles :</label>
+            <textarea
+              className="border rounded-xl p-2 w-full block"
+              rows={4}
+              cols={30}
+              placeholder="Write Captions in the Timespan..."
+              value={subtitleText}
+              onChange={handleSubtitleTextChange}
             />
-          </div>
-          <div>
-            <label className="text-bold">End Time</label>
-            <DateTime
-              className="border rounded-xl p-2 block"
-              value={endTime}
-              onChange={handleEndTimeChange}
-              dateFormat={false}
-              timeFormat="HH:mm:ss"
-            />
-          </div>
-          <label className="font-bold block"> Subtitles :</label>
-          <textarea
-            className="border rounded-xl p-2 w-full block"
-            rows={4}
-            cols={30}
-            placeholder="Write Captions in the Timespan..."
-            value={subtitleText}
-            onChange={handleSubtitleTextChange}
-          />
-          {
-            changeButton ?
+            {changeButton ? (
               <button
                 className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4 transition duration-300"
                 onClick={handleAddSubtitle}
               >
                 Add Subtitle
-              </button> :
+              </button>
+            ) : (
               <button
                 className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4 transition duration-300"
                 onClick={handleEditedItem}
               >
                 Submit changes
               </button>
-          }
-        </div>
-        <div>
-          <h2 className="font-bold text-xl mt-4">Subtitles :</h2>
-          {subtitles.map((subtitle, index) => (
-            <div key={index} className="flex">
-              <p className="font-bold w-80 max-w-3/4">
-                [{convertSecondsToTime(subtitle.startTime)} -{" "}
-                {convertSecondsToTime(subtitle.endTime)}] : {subtitle.text}
-              </p>
+            )}
+          </div>
+          <div>
+            <h2 className="font-bold text-xl mt-4">Subtitles :</h2>
+            {subtitles.map((subtitle, index) => (
+              <div key={index} className="flex">
+                <p className="font-bold w-80 max-w-3/4">
+                  [{convertSecondsToTime(subtitle.startTime)} -{" "}
+                  {convertSecondsToTime(subtitle.endTime)}] : {subtitle.text}
+                </p>
+                <button
+                  className="ml-6 text-2xl"
+                  onClick={() => handleEdit(index)}
+                >
+                  <MdOutlineEdit />
+                </button>
+                <button
+                  className="text-2xl"
+                  onClick={() => handleDelete(index)}
+                >
+                  <TiDelete />
+                </button>
+              </div>
+            ))}
+          </div>
+          {subtitles.length > 0 && (
+            <div>
               <button
-                className="ml-6 text-2xl"
-                onClick={() => handleEdit(index)}
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 mt-4"
+                onClick={handleGenerateSubtitleFile}
               >
-                <MdOutlineEdit />
-              </button>
-              <button
-                className="text-2xl"
-                onClick={() => handleDelete(index)}
-              ><TiDelete />
+                Download Subtitles File
               </button>
             </div>
-          ))}
+          )}
         </div>
-        {subtitles.length > 0 && (
-          <div>
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300 mt-4"
-              onClick={handleGenerateSubtitleFile}
-            >
-              Download Subtitles File
-            </button>
-          </div>
-        )}
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 };
 
